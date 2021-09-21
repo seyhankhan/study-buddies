9bad074ce39f35226627e035d8b2915182acd717
Seyhan Van Khan <44237992+seyhanvankhan@users.noreply.github.com>
2021-09-21 19:06:46 +0100

    Auto email invitation links when a course ends

--- studybuddy/models.py
+++ studybuddy/models.py
@@ -11,3 +11,11 @@ class Customer(Table):
 			environ.get('AIRTABLE_SHS_BASE'),
 			'Seyhan test copy'# if settings.DEBUG else 'Customers'
 		)
+
+class Offering(Table):
+  def __init__(self):
+    super().__init__(
+			environ.get('AIRTABLE_KEY'),
+			environ.get('AIRTABLE_SHS_BASE'),
+			'Offerings'
+		)
--- studybuddy/scheduler.py
+++ studybuddy/scheduler.py
@@ -1,7 +1,11 @@
 from datetime import datetime
+from json import dumps
 from os import environ
 from pytz import timezone
 
+from pyairtable.formulas import match
+
+from models import Customer, Offering
 from emails import Email, sendEmails
 from pairs import getUniquePairs
 
@@ -10,7 +14,7 @@ now = timezone("UTC").localize(datetime.now())
 ############################# EMAIL MONTHLY PAIRS #############################
 
 # Runs 1st day of every month, at xx:xx
-if True or now.day == 1:
+if now.day == 1:
 	pairs = getUniquePairs()
 	emails = []
 	for pair in pairs:
@@ -28,7 +32,43 @@ if True or now.day == 1:
 					"email3": pair[2]['fields']['Email-test'] if len(pair) == 3 else '',
 				},
 			))
-	print(len(emails), 'emails')
-	
+	print(len(emails), 'monthly pairs emails')
+
+	if settings.DEBUG:
+		sendEmails(emails[:2])
+	else:
+		sendEmails(emails)
+
+############################## EMAIL NEW ALUMNI ###############################
+
+# Runs on every end date of Offerings, at xx:xx
+offeringsEndingToday = Offering().all(view='Ending today')
+
+offeringsEndingTodayIDs = set(offering['id'] for offering in offeringsEndingToday)
+print('Offerings ending today:', [course['fields'].get('Name') for course in offeringsEndingToday])
+
+customers = Customer().all()
+emails = []
+
+for customer in customers:
+	# if they're not in any courses or already a Study Buddy
+	if 'List of Offerings' not in customer['fields'] or customer['fields'].get('Study Buddy'):
+		continue
+	alumniCourses = offeringsEndingTodayIDs.intersection(customer['fields']['List of Offerings'])
+	if len(alumniCourses) > 0:
+		print(customer['fields']['First Name'], 'is now an alum')
+		emails.append(Email(
+			to=customer['fields']['Email-test'],
+			templateID=environ.get("SENDGRID_SHS_JOINSTUDYBUDDY_TEMPLATEID"),
+			templateVariables={
+				"name": customer['fields']['First Name'],
+				"recordID": customer['id'],
+			},
+		))
+
+print(len(emails), 'invitation emails')
+
+if settings.DEBUG:
 	sendEmails(emails[:2])
-	# sendEmails(emails)
+else:
+	sendEmails(emails)
