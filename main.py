09c41cb2fdd3f4f1714f42374b62c2c83c960e7d
Seyhan Van Khan <44237992+seyhanvankhan@users.noreply.github.com>
2021-09-21 01:50:10 +0100

    Website with join form & optout

--- main.py
+++
@@ -1,8 +0,0 @@
-# Seyhan Van Khan
-# Sex Homework Society Study Buddies
-# auto match people up every month uniquely according to chosen genders and monthly prompts
-# September 2021
-
-from emails import sendPairsEmails
-
-sendPairsEmails()
