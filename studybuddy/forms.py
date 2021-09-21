a55d7bc2311273bf358a47e12e88abca3dc969b4
Seyhan Van Khan <44237992+seyhanvankhan@users.noreply.github.com>
2021-09-21 23:39:35 +0100

    Improve form styling

--- studybuddy/forms.py
+++ studybuddy/forms.py
@@ -9,5 +9,5 @@ GENDERS = [
 class JoinForm(forms.Form):
 	# name = forms.CharField(label='Name', max_length=100, required=False, disabled=True)
 	# email = forms.CharField(label='Email', max_length=30, required=False, disabled=True)
-	gender = forms.ChoiceField(label='Gender', widget=forms.RadioSelect, choices=GENDERS)
-	gendersToPairWith = forms.MultipleChoiceField(label='Genders to pair with', widget=forms.CheckboxSelectMultiple, choices=GENDERS)
+	gender = forms.ChoiceField(label='What is your gender?', widget=forms.RadioSelect, choices=GENDERS)
+	gendersToPairWith = forms.MultipleChoiceField(label='What genders would you like to pair with?', widget=forms.CheckboxSelectMultiple, choices=GENDERS)
